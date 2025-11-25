c1dd26d6148ac66a49356f2ea90045a7abfd4b5e
Loic D <loic.danj@gmail.com>
2025-11-25 13:32:20 +0100

    SEC

--- Script_SWU.js
+++ Script_SWU.js
@@ -214,7 +214,7 @@ async function run() {
         }
     }
 
-    const files = ["SWU_SOR.json", "SWU_SHD.json", "SWU_TWI.json", "SWU_JTL.json", "SWU_LOF.json", "SWU_IBH.json"];
+    const files = ["SWU_SOR.json", "SWU_SHD.json", "SWU_TWI.json", "SWU_JTL.json", "SWU_LOF.json", "SWU_IBH.json", "SWU_SEC.json"];
 
     // Exécution séquentielle
     for (let i = 0; i < files.length; i++) {
